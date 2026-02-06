ce64e976fb8f276633babee70284289df2b4748c
Mihail Markovski <misho5071@gmail.com>
2026-02-06 15:24:21 +0200

    Secure settings with env and gitignore

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -1,12 +1,15 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-*@3=#&03ufa$5#(=lz7(2*4u!66jf$7@)*c04bl)@u^ha18=rj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -20,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
